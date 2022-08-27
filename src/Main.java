101c9d713fa41d71c961e4ac21cacf8ae5c772f4
Ivanova <varia.nikitina@yandex.ru>
2022-08-27 20:41:12 +0600

    сделано

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,96 @@
 import java.util.Random;
-
 public class Main {
-    public static int []generateRandomArray(){
-        java.util.Random random = new java.unil.Random();
-        int [] arr = new int [30];
-        for ( int i = 0; i< arr.length; i++){
-            arr[i]= random.nextInt( 100_000)+100_000;
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
         }
         return arr;
-
-        public static void main ( String[]args){
-            var expenses = generateRandomArray();
-            int sum = 0;
-            for ( int i=0; i< expenses.length; i++){
-                sum + = expenses[i];
-            }
-            System.out.print("Сумма трат за месяц составила %d рублей",sum);
-        }
     }
 
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
 
+    public static void task1() {
 
+        // Задание 1
+        // Первым делом бухгалтеры попросили посчитать сумму всех выплат за месяц.
+        //  Нужно написать программу, которая решит эту задачу, и вывести в консоль результат
+        // в формате: «Сумма трат за месяц составила … рублей».
+
+        var salaries = generateRandomArray();
+        int sum = 0;
+        for (var salary : salaries) {
+            sum += salary;
+        }
+        System.out.printf("Сумма трат за месяц составила %d рублей", sum);
+    }
+
+    public static void task2() {
+
+        // Задание 2
+        // Следующая задача — найти минимальную и максимальную трату за день.
+        // Нужно написать программу, которая решит эту задачу, и вывести в консоль результат
+        //  в формате: «Минимальная сумма трат за день составила … рублей.
+        //  Максимальная сумма трат за день составила … рублей».
+
+        var salaries = generateRandomArray();
+        int max = salaries[0];
+        int min = salaries[0];
+
+        for (var salary : salaries) {
+            if (max < salary) {
+                max = salary;
+            }
+            if (min > salary) {
+                min = salary;
+            }
+        }
+        System.out.printf("Минимальная сумма трат за день составила %d рублей.Максимальная сумма трат за день составила %d рублей", min, max);
+
+    }
+
+    public static void task3() {
+        //А теперь нам нужно понять, какую в среднем сумму наша компания тратила в течение данных 30 дней.
+        //Нужно написать программу, которая посчитает среднее значение трат за месяц
+        // (то есть сумму всех трат за месяц поделить на количество дней),
+        // и вывести в консоль результат в формате: «Средняя сумма трат за месяц составила … рублей».
+        //**Важно помнить:** подсчет среднего значения может иметь остаток
+        // (то есть быть не целым, а дробным числом).
+
+        var salaries = generateRandomArray();
+        int sum = 0;
+        for (var salary : salaries) {
+            sum += salary;
+        }
+        var result = (double) sum / salaries.length;
+        System.out.println("Средняя сумма трат за месяц составила " + result + " рублей ");
+    }
+
+    public static void task4() {
+
+        // В нашей бухгалтерской книге появился баг. Что-то пошло нет так,
+        // и Ф.И.О. сотрудников начали отображаться в обратную сторону.
+        // Т. е. вместо «Иванов Иван Иванович» мы имеем «чивонавИ навИ вонавИ».
+        // Данные с именами сотрудников хранятся в виде массива символов (char[]).
+        //Напишите код, который в случае такого бага будет выводить Ф. И. О. сотрудников
+        // в корректном виде. В качестве данных для массива используйте:
+        // char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        //В результате в консоль должно быть выведено "Ivanov Ivan".
+        //**Важно**: не используйте дополнительные массивы для решения этой задачи.
+        // Необходимо корректно пройти по массиву циклом и распечатать его элементы в правильном порядке.
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+    }
 }
+
+
+
